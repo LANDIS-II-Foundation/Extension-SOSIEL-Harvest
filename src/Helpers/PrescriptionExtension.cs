5bc9124e46951819cb8dd311ea62103e3a5c9b7d
Vadim Moskvin <vadim.moskvin@yandex.by>
2019-12-26 07:27:54 +0300

    Prescription generation

---
+++ src/Helpers/PrescriptionExtension.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Landis.Library.HarvestManagement;
+using Landis.Utilities.PlugIns;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Landis.Extension.SOSIELHarvest.Helpers
+{
+    public static class PrescriptionExtension
+    {
+        private static readonly JsonSerializer Serializer;
+
+        static PrescriptionExtension()
+        {
+            Serializer = new JsonSerializer
+            {
+                Context = new StreamingContext(StreamingContextStates.All),
+                TypeNameHandling = TypeNameHandling.All,
+                PreserveReferencesHandling = PreserveReferencesHandling.All,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                Formatting = Formatting.Indented
+            };
+        }
+
+        public static Prescription Copy(this Prescription prescription)
+        {
+            return DeepClone(prescription);
+        }
+
+        private static T DeepClone<T>(T original)
+        {
+            var writer = new StringWriter();
+
+            Serializer.Serialize(new JsonTextWriter(writer), original);
+
+            var @string = writer.ToString();
+
+            return Serializer.Deserialize<T>(new JsonTextReader(new StringReader(@string)));
+        }
+    }
+
+    public class AllFieldsContractResolver : DefaultContractResolver
+    {
+        //protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        //{
+        //    var props = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+        //            .Select(f => base.CreateProperty(f, memberSerialization)).ToList();
+        //    props.ForEach(p => { p.Writable = true; p.Readable = true; });
+        //    return props;
+        //}
+
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var props = type
+                .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                .Select(p => base.CreateProperty(p, memberSerialization))
+                .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(f => base.CreateProperty(f, memberSerialization)))
+                .ToList();
+            props.ForEach(p => { p.Writable = true; p.Readable = true; });
+            return props;
+        }
+    }
+}
--- src/PlugIn.cs
+++ src/PlugIn.cs
@@ -3,17 +3,24 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using Landis.Core;
 using Landis.Extension.SOSIELHarvest.Algorithm;
 using Landis.Extension.SOSIELHarvest.Configuration;
+using Landis.Extension.SOSIELHarvest.Helpers;
 using Landis.Extension.SOSIELHarvest.Input;
 using Landis.Extension.SOSIELHarvest.Models;
 using Landis.Library.BiomassCohorts;
 using Landis.Library.HarvestManagement;
 using Landis.SpatialModeling;
 using Landis.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using SOSIEL.Configuration;
 using SOSIEL.Entities;
 using SOSIEL.Enums;
@@ -36,7 +43,8 @@ namespace Landis.Extension.SOSIELHarvest
         private LuhyLiteImplementation luhyLite;
         private BiomassHarvest.PlugIn _biomassHarvest;
         private IManagementAreaDataset _biomassHarvestAreas;
-        
+        private List<ExtendedPrescription> _extendedPrescriptions;
+
 
         private static ICore modelCore;
 
@@ -51,6 +59,7 @@ namespace Landis.Extension.SOSIELHarvest
         public PlugIn()
             : base(ExtensionName, ExtType)
         {
+            _extendedPrescriptions = new List<ExtendedPrescription>();
         }
 
         //---------------------------------------------------------------------
@@ -69,13 +78,14 @@ namespace Landis.Extension.SOSIELHarvest
             var sheParameterParser = new SheParameterParser();
             _sheParameters = Data.Load(dataFile, sheParameterParser);
 
-            ModelCore.UI.WriteLine("  Loading parameters from {0}", _sheParameters.SosielInitializationFileName);
-            var sosielParameterParser = new SosielParameterParser();
-            _sosielParameters = Data.Load(_sheParameters.SosielInitializationFileName, sosielParameterParser);
+            //ModelCore.UI.WriteLine("  Loading parameters from {0}", _sheParameters.SosielInitializationFileName);
+            //var sosielParameterParser = new SosielParameterParser();
+            //_sosielParameters = Data.Load(_sheParameters.SosielInitializationFileName, sosielParameterParser);
 
             if (_sheParameters.Mode == 2)
             {
-                ModelCore.UI.WriteLine("  Loading parameters from {0}", _sheParameters.BiomassHarvestInitializationFileName);
+                ModelCore.UI.WriteLine("  Loading parameters from {0}",
+                    _sheParameters.BiomassHarvestInitializationFileName);
                 _biomassHarvest = new BiomassHarvest.PlugIn();
                 _biomassHarvest.LoadParameters(_sheParameters.BiomassHarvestInitializationFileName, ModelCore);
             }
@@ -85,12 +95,12 @@ namespace Landis.Extension.SOSIELHarvest
 
         public override void Initialize()
         {
-            ModelCore.UI.WriteLine("Initializing {0}...", Name); 
+            ModelCore.UI.WriteLine("Initializing {0}...", Name);
             SiteVars.Initialize();
 
             Timestep = _sheParameters.Timestep;
 
-            _configuration = ConfigurationParser.MakeConfiguration(_sosielParameters);
+            //_configuration = ConfigurationParser.MakeConfiguration(_sosielParameters);
 
             //// Read in (input) Agent Configuration Json File here:
             //ModelCore.UI.WriteLine("  Loading agent parameters from {0}", parameters.InputJson);
@@ -123,15 +133,17 @@ namespace Landis.Extension.SOSIELHarvest
                 _biomassHarvest.Initialize();
 
                 var biomassHarvestPluginType = _biomassHarvest.GetType();
-                var prescriptionField = biomassHarvestPluginType.GetField("managementAreas", BindingFlags.Instance | BindingFlags.NonPublic);
+                var prescriptionField = biomassHarvestPluginType.GetField("managementAreas",
+                    BindingFlags.Instance | BindingFlags.NonPublic);
                 if (prescriptionField == null)
                     throw new Exception();
-                _biomassHarvestAreas = (IManagementAreaDataset)prescriptionField.GetValue(_biomassHarvest);
+                _biomassHarvestAreas = (IManagementAreaDataset) prescriptionField.GetValue(_biomassHarvest);
 
-                var parametersField = biomassHarvestPluginType.GetField("parameters", BindingFlags.Static | BindingFlags.NonPublic);
+                var parametersField =
+                    biomassHarvestPluginType.GetField("parameters", BindingFlags.Static | BindingFlags.NonPublic);
                 if (parametersField == null)
                     throw new Exception();
-                _biomassHarvestParameters = (BiomassHarvest.Parameters)parametersField.GetValue(_biomassHarvest);
+                _biomassHarvestParameters = (BiomassHarvest.Parameters) parametersField.GetValue(_biomassHarvest);
             }
         }
 
@@ -140,19 +152,76 @@ namespace Landis.Extension.SOSIELHarvest
             //run SOSIEL algorithm
             //luhyLite.Run(luhyModel);
 
+            if (iteration == 1)
+            {
+                GenerateNewPrescription("newPrescription", "HarvestArea", 0.5, "MM1-1_DO1");
+                ApplyNewPrescription("newPrescription");
+            }
+
             if (_sheParameters.Mode == 2)
                 _biomassHarvest.Run();
 
             iteration++;
         }
 
-        private void ChangePrescription()
+        private void GenerateNewPrescription(string newName, string parameter, dynamic value, string basedOn)
+        {
+            var originalPrescription = _biomassHarvestParameters.Prescriptions.First(p => p.Name.Equals(basedOn));
+
+            var newPrescription = originalPrescription.Copy();
+
+            var type = newPrescription.GetType();
+            var nameField = type.GetField("name", BindingFlags.Instance | BindingFlags.NonPublic);
+            nameField.SetValue(newPrescription, newName);
+
+            var rankingMethodField = type.GetField("rankingMethod", BindingFlags.Instance | BindingFlags.NonPublic);
+            rankingMethodField.SetValue(newPrescription, originalPrescription.StandRankingMethod);
+
+            var cohortCutterField = type.GetField("cohortCutter", BindingFlags.Instance | BindingFlags.NonPublic);
+            var cohortCutter = cohortCutterField.GetValue(originalPrescription);
+            cohortCutterField.SetValue(newPrescription, cohortCutter);
+
+            AppliedPrescription appliedPrescription = null;
+
+            foreach (var managementArea in _biomassHarvestParameters.ManagementAreas)
+            {
+                appliedPrescription =
+                    managementArea.Prescriptions.FirstOrDefault(p => p.Prescription.Name.Equals(basedOn));
+
+                if (appliedPrescription != null)
+                    break;
+            }
+
+            var areaToHarvest = appliedPrescription.PercentageToHarvest;
+            var standsToHarvest = appliedPrescription.PercentStandsToHarvest;
+            var beginTime = appliedPrescription.BeginTime;
+            var endTime = appliedPrescription.EndTime;
+
+            switch (parameter)
+            {
+                case "HarvestArea":
+                    areaToHarvest = new Percentage(value);
+                    break;
+                case "CohortsRemovedProportionAdjustment":
+                    
+                    break;
+            }
+
+            _extendedPrescriptions.Add(new ExtendedPrescription(newPrescription, areaToHarvest, standsToHarvest,
+                beginTime, endTime));
+        }
+
+        private void ApplyNewPrescription(string prescriptionName)
         {
             foreach (var biomassHarvestArea in _biomassHarvestAreas)
             {
                 biomassHarvestArea.Prescriptions.Clear();
-                var prescription = _biomassHarvestParameters.Prescriptions.First(p => p.Name.Equals("MM2_DO2"));
-                biomassHarvestArea.ApplyPrescription(prescription, new Percentage(0.2), new Percentage(1), 0, 100);
+                var extendedPrescription =
+                    _extendedPrescriptions.First(p => p.Prescription.Name.Equals(prescriptionName));
+                biomassHarvestArea.ApplyPrescription(extendedPrescription.Prescription,
+                    new Percentage(extendedPrescription.HarvestAreaPercent),
+                    new Percentage(extendedPrescription.HarvestStandsAreaPercent), extendedPrescription.StartTime,
+                    extendedPrescription.EndTime);
                 biomassHarvestArea.FinishInitialization();
             }
         }
@@ -164,10 +233,9 @@ namespace Landis.Extension.SOSIELHarvest
             double siteBiomass = 0;
             if (SiteVars.Cohorts[site] != null)
                 foreach (ISpeciesCohorts speciesCohorts in SiteVars.Cohorts[site])
-                    foreach (ICohort cohort in speciesCohorts)
-                        siteBiomass += cohort.Biomass;
+                foreach (ICohort cohort in speciesCohorts)
+                    siteBiomass += cohort.Biomass;
             return siteBiomass;
         }
-       
     }
 }
