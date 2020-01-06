b6a2a41a1ea6760e140419d4972a07ed2dffa251
aleksandr.karifanov <aleksandr.karifanov@effective-soft.com>
2020-01-06 22:14:02 +0300

    add missed output model

---
+++ src/Output/FMDOUsageOutput.cs
@@ -0,0 +1,15 @@
+﻿namespace Landis.Extension.SOSIELHarvest.Output
+{
+    public class FMDOUsageOutput
+    {
+        public int Iteration { get; set; }
+
+        public string ManagementArea { get; set; }
+
+        public string[] ActivatedDOValues { get; set; }
+
+        public string[] ActivatedDO { get; set; }
+
+        public int TotalNumberOfDO { get; set; }
+    }
+}
